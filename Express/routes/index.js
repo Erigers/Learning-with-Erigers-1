bf30c82101c04a3c7f20ca7d13e66eacf32cdf42
Amarius Alla <alla.amarius@gmail.com>
2020-11-11 02:18:02 +0100

    Update index.js

--- Express/routes/index.js
+++ Express/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 let todos = require('../public/todos.json');
 const {check , validationResult} = require('express-validator');
+const { response } = require('express');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,21 +21,19 @@ router.get('/', (req, res) => {
  * @description: Text
  * Validation done with express-validator pacakge
  */
-router.post('/:name/:description', [check('name').notEmpty() , check('description').notEmpty(), check('name').isAlpha()], 
+router.post('/:name/:description', [check('name').notEmpty() , check('description').notEmpty()] , 
 async (req, res, next) => {
   try {
     // validate if the parameters don't exist throw a meaningful error , else perform the request
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-      throw (new Error(`Error reading the data`));  
-    }
+    if(!errors.isEmpty()) {throw (new Error(`Error reading the data`));}  
     else{
       const { name, description } = req.params;
       todos.push({name:name , description: description});
       res.send(todos);
     }
-  } catch(err) {
-      response.send(err);
+  } catch(err){
+      response.send(`An error happened: ${err}`);
     }
 });
 
@@ -42,13 +41,25 @@ async (req, res, next) => {
  * Edit data with post
  * 
  */
-
-router.post('/edit/:id/:name/:descritpion', check())
+router.put('/:id/:name/:description', [check('name').notEmpty() , check('description').notEmpty()] , 
+async (req, res , next) =>{
+  try{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) throw (new Error(`Error updating the data`));  
+    else{
+      const { id , name, description } = req.params;
+      const el = {id: id, name: name, descritpion: description};
+      todos[el.id-1] = el;
+      res.send(todos);
+    }  
+  }catch(err){
+    res.send(`${err}`);
+  }
+});
 
 /**
  * Delete a Todo param , it deletes the id requested and also doesn't leave an empty space to memory
  */
-
  let deleted = 0;
 router.delete('/:id',(request, response) => {
   const { id } = request.params;
@@ -69,7 +80,7 @@ router.delete('/:id',(request, response) => {
     }
   } 
   catch(err){
-    response.send(`Upss something went wrong deleting your element ${err}`);
+    response.send(`Upss something went wrong deleting your element: ${err}`);
   }
 });
 
