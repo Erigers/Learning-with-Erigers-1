4340b3665eda968b25b4147a020d8ecf6b403224
Amarius Alla <alla.amarius@gmail.com>
2020-11-12 01:40:58 +0100

    Structured Project
    Project after seperating on diferent files giving structure to the project.

---
+++ Express/controllers/TodoController.js
@@ -0,0 +1,64 @@
+let todos = require('../public/todos.json');
+const {validationResult} = require('express-validator');
+const { response } = require('express');
+
+module.exports = function (req, res){
+    this.getTodos = (req, res) => {
+        console.log(res, 'res');
+        res.send(todos);
+      }
+
+    this.createTodo = async (req, res, next) => {
+        try {
+          // validate if the parameters don't exist throw a meaningful error , else perform the request
+          
+            const { name, title } = req.body;
+            todos.push({name, title});
+            res.send(todos);
+          
+        } catch(err){
+            res.send(`An error happened: ${err}`);
+          }
+      }
+
+    this.updateTodo = async (req, res , next) =>{
+        try{
+         
+            const id = req.params.id; 
+            const {name, title } = req.body;
+            const el = {id, name, title};
+            todos[el.id-1] = el;
+            res.send(todos);
+            
+        }catch(err){
+          res.send(`${err}`);
+        }
+      }  
+      
+
+        let deleted = 0;
+    this.deleteTodo = (request, response) => {
+        const { id } = request.params;
+        try{
+          if(id > todos.length || id < 0){
+            throw (new Error('Id was not valid'));
+          }  
+          else{
+            todos = todos.filter((item) =>{ 
+              return item.id !== id;  
+           });
+           deleted++;
+           delete todos[id-deleted];
+           todos = todos.filter(function (el) {
+            return el != null;
+          });
+           response.send(todos);
+          }
+        } 
+        catch(err){
+          response.send(`Upss something went wrong deleting your element: ${err}`);
+        }
+      } 
+}
+
+
---
+++ Express/routes/todos.js
@@ -0,0 +1,24 @@
+const TodoController = require('../controllers/TodoController')
+var express = require('express');
+var router = express.Router();
+
+
+    router.get('/', (req, res) => {
+        //console.log(res, 'res');
+        new TodoController().getTodos(req, res);
+      });
+      
+      router.post('/create',  (req,res) =>{
+          new TodoController().createTodo(req, res);
+      } );
+      
+      
+      router.put('/:id', (req, res) =>{
+         new TodoController().updateTodo(req, res);
+      });
+      
+      
+      router.delete('/:id' , (req, res) =>{
+          new TodoController().deleteTodo(req,res);
+      });
+module.exports = router
