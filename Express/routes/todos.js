7e842ad4d843156a3c933dd17c488048235ccfcd
Amarius Alla <alla.amarius@gmail.com>
2020-11-12 01:56:26 +0100

    Folder added for Project Sturcture

--- Express/routes/todos.js
+++ Express/routes/todos.js
@@ -1,24 +1,21 @@
-const TodoController = require('../controllers/TodoController')
-var express = require('express');
+const TodoController = require("../controllers/TodoController");
+var express = require("express");
 var router = express.Router();
 
+router.get("/", (req, res) => {
+  //console.log(res, 'res');
+  new TodoController().getTodos(req, res);
+});
 
-    router.get('/', (req, res) => {
-        //console.log(res, 'res');
-        new TodoController().getTodos(req, res);
-      });
-      
-      router.post('/create',  (req,res) =>{
-          new TodoController().createTodo(req, res);
-      } );
-      
-      
-      router.put('/:id', (req, res) =>{
-         new TodoController().updateTodo(req, res);
-      });
-      
-      
-      router.delete('/:id' , (req, res) =>{
-          new TodoController().deleteTodo(req,res);
-      });
-module.exports = router
+router.post("/create", (req, res) => {
+  new TodoController().createTodo(req, res);
+});
+
+router.put("/:id", (req, res) => {
+  new TodoController().updateTodo(req, res);
+});
+
+router.delete("/:id", (req, res) => {
+  new TodoController().deleteTodo(req, res);
+});
+module.exports = router;
